5383f98e3e461fd3201cc489a04684f727c1eecd
evanchristians <evanryk@gmail.com>
2020-12-05 17:09:29 +0200

    begun adding pwa support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,10 @@ export default class Document extends NextDocument {
           rel="stylesheet"
         />
         <script src="https://kit.fontawesome.com/ce5d40ef27.js" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#7864F7" />
+        <link rel="apple-touch-icon" href="/logo-96x96.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="#7864F7" />
         <body>
           <ColorModeScript />
           <Main />
